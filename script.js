61095eca289f0f9a79bfbd0d3dc798f7f68ca767
littbrian <47404512+littbrian@users.noreply.github.com>
2023-05-23 15:35:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+function addFunction() {
+    var currentCount = document.getElementById('count');
+    var number = currentCount.innerHTML;
+    number++;
+    currentCount.innerHTML = number;
+}
+
+function subFunction() {
+    var currentCount = document.getElementById('count');
+    var number = currentCount.innerHTML;
+    number--;
+    currentCount.innerHTML = number;
+}
+
+    function addFunctionn() {
+    var currentCount = document.getElementById('count1');
+    var number = currentCount.innerHTML;
+    number++;
+    currentCount.innerHTML = number;
+}
+
+function subFunctionn() {
+    var currentCount = document.getElementById('count1');
+    var number = currentCount.innerHTML;
+    number--;
+    currentCount.innerHTML = number;
+}
+
+
